8df6ca25ef7d00fafdaa15f866fb7a1d1075f72d
Iron <jjarevalog@ufpso.edu.co>
2024-11-02 13:55:55 -0500

    Send

---
+++ Ejercicios/P.O.O/Plantilla.java
@@ -0,0 +1,10 @@
+/*
+*/
+
+public class Plantilla {
+    public static void main (String[] args) {
+        //Inicio algoritmo
+        
+        //Fin algoritmo
+    }
+}
---
+++ Ejercicios/P.O.O/Sistema_Biblioteca.java
@@ -0,0 +1,26 @@
+/*
+Crear un sistema orientado a objetos para gestionar una biblioteca. 
+El sistema debe manejar libros, usuarios y transacciones de préstamo.
+*/
+
+public class Book{
+
+
+}
+
+public class User{
+
+}
+
+public class Tranction{
+    
+}
+
+
+public class Sistema_Biblioteca {
+    public static void main (String[] args) {
+        //Inicio algoritmo
+        
+        //Fin algoritmo
+    }
+}
